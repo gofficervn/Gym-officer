e7405db42d31645f392e063df28c757f9cceeaf0
quyctd <skyvp98@gmail.com>
2017-11-09 00:12:50 +0700

    .

--- app.py
+++ app.py
@@ -55,7 +55,72 @@ def search():
     else:
         listbaitap = BaiTap.objects(time=time, purpose = purpose, space = space)
 
-    return render_template("baitap.html", listbaitap = listbaitap)
+    return render_template("ketqua.html", listbaitap = listbaitap)
+
+@app.route("/access")
+def access():
+    return render_template("access.html")
+
+@app.route("/admin", methods =["POST"])
+def admin():
+    listbaitap = BaiTap.objects()
+    if request.form["email"]== "admin" and request.form["pass"] =="admin":
+        return render_template('admin.html',listbaitap = listbaitap)
+    else:
+        return render_template("homepage.html")
+
+
+@app.route('/addbaitap')
+def adbaitap():
+    return render_template('addbaitap.html')
+
+
+@app.route('/baitap/<baitap_id>', methods=["GET"])
+def baitap(baitap_id):
+    baitap = BaiTap.objects().with_id(baitap_id)
+    return render_template('baitap.html', baitap= baitap)
+
+@app.route('/deletebaitap/<baitap_id>')
+def deletebaitap(baitap_id):
+    listbaitap = BaiTap.objects()
+
+    baitap = BaiTap.objects().with_id(baitap_id)
+    if baitap is None:
+        print("Not found")
+    else:
+        baitap.delete()
+
+    return render_template('admin.html',listbaitap = listbaitap)
+
+@app.route('/updatebaitap/<baitap_id>',methods=['GET','POST'])
+def updatebaitap(baitap_id):
+    listbaitap = BaiTap.objects()
+    baitap = BaiTap.objects().with_id(baitap_id)
+    if request.method == "GET":
+        return render_template('updatebaitap.html',baitap = baitap)
+    elif request.method == "POST":
+
+        form = request.form
+        name = form['name']
+        time = form['time']
+        purpose = form['purpose']
+        space = form['space']
+        description = form['description']
+        image = form['image']
+        clip = form['clip']
+
+        baitap.update(set__name = name,set__time = time, set__purpose = purpose, set__space = space
+                        , set__description = description,set__image = image, set__clip = clip,
+        )
+
+    return render_template('admin.html',listbaitap = listbaitap)
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+@app.route("/aboutus")
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
   app.run(debug=True)
